866f08171d20628fd0773abc657684a2af3d4077
Dougal J. Sutherland <dougal@gmail.com>
2015-08-03 20:16:32 -0400

    initial commit

---
+++ conda-worker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Wrapper because launchctl doesn't set PATH properly
+# http://www.openradar.me/18945659
+
+QUEUE=$1
+ANACONDA=${2:-HOME/miniconda}
+
+export PATH="$ANACONDA/bin:$PATH"
+anaconda-build -t ~/.binstar.token worker $QUEUE
